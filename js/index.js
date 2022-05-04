3cd676d61cd176d08bce1a40909dda2f08000c26
MarcosHPaiva <103915212+MarcosHPaiva@users.noreply.github.com>
2022-05-04 13:12:07 -0300

    Mudancas no cardapio e destaque.
    Destaque agora é promoções e o cardápio foi movido pra sua própria página com mesmo nome. Atendendo assim ao menu dropdown, cardápio removido e adicionado em sua própria página.

--- js/index.js
+++ js/index.js
@@ -6,6 +6,14 @@ function preload() {
         preloading.src=imgs[i];
     }
 }
+function preloadPromo() {
+    imags=Array("img/promo1.jpg", "img/promo2.jpg", "img/promo3.jpg");
+    imgQtde=imags.length;
+    for(i=0;i<imgQtde;i++) {
+        var preloading=new Image();
+        preloading.src=imags[i];
+    }
+}
 
 function iniciaSlider() {
     preload();
@@ -15,29 +23,72 @@ function iniciaSlider() {
     tr=true;
     carregaFoto("img/f1.jpg");
     document.getElementById("img").addEventListener("transitionend", fimTr);
+    tmr=setInterval(trocaFoto, 4000);
 }
 
+function iniciaPromocoes(){
+    preloadPromo();
+    maxi=3;
+    mini=1;
+    fix=min;
+    tra=true;
+    carregaPromo("img/promo1.jpg");
+    document.getElementById("imgPromo").addEventListener("transitionend", fimTra);
+    timr=setInterval(trocaPromo, 4000);
+}
+
+function trocaFoto(){
+    fi++;
+    if(fi>max){
+        fi=min;
+    }
+    carregaFoto("img/f"+fi+".jpg");
+}
+
+function trocaPromo(){
+    fix++;
+    if(fix>maxi){
+        fix=mini;
+    }
+    carregaPromo("img/promo"+fix+".jpg");
+}
 function fimTr(){
     tr=true;
+}
+function fimTra(){
+    tra=true;
 }
 
 function carregaFoto(foto) {
     document.getElementById("img").style.backgroundImage="URL("+foto+")";
 }
 
-function prox(){
+function carregaPromo(promo) {
+    document.getElementById("imgPromo").style.backgroundImage="URL("+promo+")";
+}
 
-    fi++;
+function trocaPromocao(dra){
+    clearInterval(timr);
+    fix+=dra;
+    if(fix>maxi){
+        fix=mini;
+    }
+    if(fix<mini){
+        fix=maxi;
+    }
+    carregaPromo("img/promo"+fix+".jpg");
+    timr=setInterval(trocaPromo, 4000); 
+}   
+
+function troca(dr){
+    clearInterval(tmr);
+    fi+=dr;
     if(fi>max){
         fi=min;
     }
-    carregaFoto("img/f"+fi+".jpg");
-    
-}
-function ant(){
-    fi--;
     if(fi<min){
         fi=max;
     }
     carregaFoto("img/f"+fi+".jpg");
-}
+    tmr=setInterval(trocaFoto, 4000); 
+}   
